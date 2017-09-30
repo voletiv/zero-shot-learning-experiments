49fb7cffe4ceb079bcff00259098d6b6f90c31a1
Vikram Voleti <vikky2904@gmail.com>
2017-10-01 00:23:20 +0530

    Correcting params files

--- GRIDcorpus/grid_params.py
+++ GRIDcorpus/grid_params.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import sys
 
@@ -6,15 +5,24 @@ import sys
 # DIRECTORIES
 #############################################################
 
-# Current directory
-CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+# GRID directory
+GRID_DIR = os.path.dirname(os.path.realpath(__file__))
+if GRID_DIR not in sys.path:
+    sys.path.append(GRID_DIR)
 
-# Lip reader directory
-LIPREADER_DIR = os.path.normpath(os.path.join(CURR_DIR, '../Lipreader'))
-sys.path.append(LIPREADER_DIR)
+# Root directory with common functions
+ROOT_DIR = os.path.normpath(os.path.join(GRID_DIR, '..'))
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
 
 # GRIDcorpus dataset directory
-GRID_DIR = '/media/voletiv/01D2BF774AC76280/Datasets/GRIDcorpus'
+GRID_DATA_DIR = '/media/voletiv/01D2BF774AC76280/Datasets/GRIDcorpus'
+
+#############################################################
+# IMPORT
+#############################################################
+
+from common_params import *
 
 #############################################################
 # PARAMETERS FOR GRIDCORPUS
@@ -38,5 +46,25 @@ NUM_OF_UNIQUE_WORDS = 53     # including silent and short pause
 # wordsVocabSize = (nOfUniqueWords - 2) + 1
 GRID_WORDS_VOCAB_SIZE = NUM_OF_UNIQUE_WORDS - 2
 
-# # wordIdx dictionary
-# WORD_IDX = np.load(os.path.join(CURR_DIR, "wordIdx.npy")).item()
+# Actually, GRID_VOCAB_SIZE = 51,
+# but 'a' is missing from word2vec vocab,
+# so GRID_VOCAB_SIZE is made 50
+GRID_VOCAB_SIZE = 50
+
+# grid_vocab list
+GRID_VOCAB_LIST_FILE = os.path.join(GRID_DIR, 'grid_vocabulary.txt')
+
+
+#############################################################
+# LOAD VOCAB LIST
+#############################################################
+
+def load_gridcorpus_vocab_list(GRID_VOCAB_LIST_FILE):
+    grid_vocab = []
+    with open(GRID_VOCAB_LIST_FILE) as f:
+        for line in f:
+            word = line.rstrip().split()[-1]
+            grid_vocab.append(word)
+    return grid_vocab
+
+GRID_VOCAB = load_gridcorpus_vocab_list(GRID_VOCAB_LIST_FILE)
---
+++ LRW/lrw_params.py
@@ -0,0 +1,45 @@
+import os
+import sys
+
+#############################################################
+# DIRECTORIES
+#############################################################
+
+# LRW directory
+LRW_DIR = os.path.dirname(os.path.realpath(__file__))
+if LRW_DIR not in sys.path:
+    sys.path.append(LRW_DIR)
+
+# Root directory with common functions
+ROOT_DIR = os.path.normpath(os.path.join(LRW_DIR, '..'))
+if ROOT_DIR not in sys.path:
+    sys.path.append(ROOT_DIR)
+
+#############################################################
+# IMPORT
+#############################################################
+
+from common_params import *
+
+#############################################################
+# PARAMETERS FOR LRW
+#############################################################
+
+LRW_VOCAB_SIZE = 500
+
+LRW_VOCAB_LIST_FILE = os.path.join(LRW_DIR, 'lrw_vocabulary.txt')
+
+#############################################################
+# LOAD VOCAB LIST
+#############################################################
+
+
+def load_lrw_vocab_list(LRW_VOCAB_LIST_FILE):
+    grid_vocab = []
+    with open(LRW_VOCAB_LIST_FILE) as f:
+        for line in f:
+            word = line.rstrip().lower()
+            grid_vocab.append(word)
+    return grid_vocab
+
+LRW_VOCAB = load_lrw_vocab_list(LRW_VOCAB_LIST_FILE)
