d594f78c5c91cb3570f666a58cd903e9a407a5cc
Vikram Voleti <vikky2904@gmail.com>
2017-10-01 00:44:38 +0530

    Shifted functions to grid_functions.py

--- GRIDcorpus/grid_functions.py
+++ GRIDcorpus/grid_functions.py
@@ -6,6 +6,144 @@ import tqdm
 from grid_params import *
 from LSTM_lipreader_function import *
 
+
+#############################################################
+# MAKE TRAINING AND TESTING DATA
+#############################################################
+
+
+def make_train_test_siInV_siOOV_data(train_num_of_words):
+
+    ########################################
+    # Read GRIDcorpus directories, etc.
+    ########################################
+
+    train_val_dirs, train_val_word_numbers, train_val_words, train_val_word_idx, \
+        si_dirs, si_word_numbers, si_words, si_word_idx \
+        = get_train_val_si_dirs_wordnumbers_words_wordidx()
+
+    ########################################
+    # Assign Training and Testing Directories
+    ########################################
+
+    # Train data - speakers speaking certain words
+    # Test data - same speakers speaking other words
+    # SI_Test data - different speakers speaking other words
+
+    all_word_idx = np.arange(GRID_VOCAB_SIZE)
+
+    # Choose words to keep in training data - training words
+    np.random.seed(29)
+    training_word_idx = np.random.choice(all_word_idx, train_num_of_words)
+
+    # Make the rest of the words as part of testing data - testing words
+    testing_word_idx = np.delete(all_word_idx, train_word_idx)
+
+    # Choose those rows in data that contain training words
+    training_data_idx = np.array([i for i in range(
+        len(train_val_words)) if train_val_word_idx[i] in training_word_idx])
+
+    # Choose the rest of the rows as testing data
+    testing_data_idx = np.delete(
+        np.arange(len(train_val_words)), training_data_idx)
+
+    # Choose those rows in data that contain training words
+    si_in_vocab_data_idx = np.array(
+        [i for i in range(len(si_words)) if si_word_idx[i] in training_word_idx])
+
+    # Choose the rest of the rows as testing data
+    si_oov_data_idx = np.delete(np.arange(len(si_words)), si_in_vocab_data_idx)
+
+    # TRAINING DATA
+    training_dirs = train_val_dirs[training_data_idx]
+    training_word_numbers = train_val_word_numbers[training_data_idx]
+    training_words = train_val_words[training_data_idx]
+    training_word_idx = training_word_idx[training_data_idx]
+
+    # (SPEAKER-DEPENDENT) TESTING DATA
+    testing_dirs = train_val_dirs[testing_data_idx]
+    testing_word_numbers = train_val_word_numbers[testing_data_idx]
+    testing_words = train_val_words[testing_data_idx]
+    testing_word_idx = training_word_idx[testing_data_idx]
+
+    # SPEAKER-INDEPENDENT IN-VOCAB DATA
+    si_in_vocab_dirs = si_dirs[si_in_vocab_data_idx]
+    si_in_vocab_word_numbers = si_word_numbers[si_in_vocab_data_idx]
+    si_in_vocab_words = si_words[si_in_vocab_data_idx]
+    si_in_vocab_word_idx = si_word_idx[si_in_vocab_data_idx]
+
+    # SPEAKER-INDEPENDENT OOV DATA
+    si_oov_dirs = si_dirs[si_oov_data_idx]
+    si_oov_word_numbers = si_word_numbers[si_oov_data_idx]
+    si_oov_words = si_words[si_oov_data_idx]
+    si_oov_word_idx = si_word_idx[si_oov_data_idx]
+
+    return training_dirs, training_word_numbers, training_words, \
+        training_word_idx, testing_dirs, testing_word_numbers, testing_words, \
+        testing_word_idx, si_in_vocab_dirs, si_in_vocab_word_numbers, \
+        si_in_vocab_words, si_in_vocab_word_idx, si_oov_dirs, \
+        si_oov_word_numbers, si_oov_words, si_oov_word_idx
+
+
+def get_train_val_si_dirs_wordnumbers_words_wordidx(
+    trainValSpeakersList=[1, 2, 3, 4, 5, 6, 7, 10],
+    siList=[13, 14]
+    ):
+
+    ########################################
+    # Read GRIDcorpus directories
+    ########################################
+
+    train_val_dirs, train_val_word_numbers, train_val_words, \
+        si_dirs, si_word_numbers, si_words \
+        = load_speakerdirs_wordnums_words_lists(trainValSpeakersList, siList)
+
+    train_val_dirs = np.array(train_val_dirs)
+    train_val_word_numbers = np.array(train_val_word_numbers)
+    train_val_words = np.array(train_val_words)
+
+    si_dirs = np.array(si_dirs)
+    si_word_numbers = np.array(si_word_numbers)
+    si_words = np.array(si_words)
+
+    ########################################
+    # Make Word Idx
+    # - map words to their index in vocab
+    ########################################
+
+    train_val_word_idx = -np.ones((len(train_val_words)))
+    for i in range(len(train_val_words)):
+        if train_val_words[i] in GRID_VOCAB:
+            train_val_word_idx[i] = GRID_VOCAB.index(train_val_words[i])
+
+    si_word_idx = -np.ones((len(si_words)))
+    for i in range(len(si_words)):
+        if si_words[i] in GRID_VOCAB:
+            si_word_idx[i] = GRID_VOCAB.index(si_words[i])
+
+    ########################################
+    # Remove rows corresponding to
+    ## words not in vocab ('a')
+    ########################################
+
+    train_val_rows_to_keep = train_val_word_idx != -1
+
+    train_val_dirs = train_val_dirs[train_val_rows_to_keep]
+    train_val_word_numbers = train_val_word_numbers[train_val_rows_to_keep]
+    train_val_words = train_val_words[train_val_rows_to_keep]
+    train_val_word_idx = train_val_word_idx[train_val_rows_to_keep]
+
+    si_rows_to_keep = si_word_idx != -1
+
+    si_dirs = si_dirs[si_rows_to_keep]
+    si_word_numbers = si_word_numbers[si_rows_to_keep]
+    si_words = si_words[si_rows_to_keep]
+    si_word_idx = si_word_idx[si_rows_to_keep]
+
+    return train_val_dirs, train_val_word_numbers, train_val_words, \
+        train_val_word_idx, si_dirs, si_word_numbers, si_words, si_word_idx
+
+
 #############################################################
 # LOAD SPEAKER_DIRS, WORD_NUMBERS, WORDS
 #############################################################
@@ -19,7 +157,8 @@ def load_speakerdirs_wordnums_words_lists(trainValSpeakersList=[1, 2, 3, 4, 5, 6
     trainValWords = []
     # For each speaker
     for speaker in tqdm.tqdm(sorted((trainValSpeakersList))):
-        speakerDir = os.path.join(GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
+        speakerDir = os.path.join(
+            GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
         # print(speakerDir)
         # List of all videos for each speaker
         vidDirs = sorted(glob.glob(os.path.join(speakerDir, '*/')))
@@ -45,7 +184,8 @@ def load_speakerdirs_wordnums_words_lists(trainValSpeakersList=[1, 2, 3, 4, 5, 6
     siWords = []
     # For each speaker
     for speaker in tqdm.tqdm(sorted((siList))):
-        speakerDir = os.path.join(GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
+        speakerDir = os.path.join(
+            GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
         # print(speakerDir)
         # List of all videos for each speaker
         vidDirs = sorted(glob.glob(os.path.join(speakerDir, '*/')))
@@ -76,7 +216,8 @@ def load_speakerdirs_wordnums_words_lists(trainValSpeakersList=[1, 2, 3, 4, 5, 6
 
 def load_LSTM_lipreader_encoder():
     LSTMLipReaderModel, LSTMLipreaderEncoder, fileNamePre = make_LSTM_lipreader_model()
-    LSTMLipReaderModel.load_weights(os.path.join(LIPREADER_DIR, 'LSTMLipReader-revSeq-Mask-LSTMh256-LSTMactivtanh-depth2-enc64-encodedActivrelu-Adam-1e-03-GRIDcorpus-s0107-09-tMouth-valMouth-NOmeanSub-epoch078-tl0.2384-ta0.9224-vl0.5184-va0.8503-sil4.9063-sia0.2393.hdf5'))
+    LSTMLipReaderModel.load_weights(os.path.join(
+        LIPREADER_DIR, 'LSTMLipReader-revSeq-Mask-LSTMh256-LSTMactivtanh-depth2-enc64-encodedActivrelu-Adam-1e-03-GRIDcorpus-s0107-09-tMouth-valMouth-NOmeanSub-epoch078-tl0.2384-ta0.9224-vl0.5184-va0.8503-sil4.9063-sia0.2393.hdf5'))
     return LSTMLipreaderEncoder
 
 
--- GRIDcorpus/grid_zsl.py
+++ GRIDcorpus/grid_zsl.py
@@ -5,7 +5,7 @@ from grid_params import *
 from grid_functions import *
 
 ########################################
-## ZSL equations
+# ZSL equations
 ########################################
 
 # minimise L(X^T * V * S, Y) + Ω(V)
@@ -13,34 +13,34 @@ from grid_functions import *
 # === dxa = (dxd) . dxm . mxz . zxa . (axa)
 
 ########################################
-## Read embedding matrices
+# Read embedding matrices
 ########################################
 
 # class_to_attr_matrix ===  class_dim x attr_dim
 
 # word2vec
 class_to_attr_matrix_word2vec = np.load(os.path.join(CURR_DIR,
-    'grid_embedding_matrix_word2vec.npy'))
+                                                     'grid_embedding_matrix_word2vec.npy'))
 
 # fasttext
 class_to_attr_matrix_fasttext = np.load(os.path.join(CURR_DIR,
-    'grid_embedding_matrix_fasttext.npy'))
+                                                     'grid_embedding_matrix_fasttext.npy'))
 
 ########################################
-## Read LipReader
+# Load LipReader
 ########################################
 
 LSTMLipreaderEncoder = load_LSTM_lipreader_encoder()
 
 ########################################
-## Vars
+# Vars
 ########################################
 
 # Number of training classes (words)
 z_vals = np.array([10, 20, 30, 40])
 
 # Number of test classes (OOV words)
-t_vals = 50 - z_vals
+t_vals = GRID_VOCAB_SIZE - z_vals
 
 # Number of atributes === word2vec dim
 attr_dim = matrix_class_to_attr.shape[1]
@@ -49,113 +49,15 @@ train_num_of_words = z_vals[0]
 test_num_of_words = t_vals[0]
 
 ########################################
-## Read GRIDcorpus directories
+# Make Training and Testing Data
 ########################################
 
-train_val_dirs, train_val_word_numbers, train_val_words, \
-        si_dirs, si_word_numbers, si_words \
-    = load_speakerdirs_wordnums_words_lists(trainValSpeakersList \
-                                            = [1, 2, 3, 4, 5, 6, 7, 10],
-                                        siList = [13, 14])
-
-train_val_dirs = np.array(train_val_dirs)
-train_val_word_numbers = np.array(train_val_word_numbers)
-train_val_words = np.array(train_val_words)
-
-si_dirs = np.array(si_dirs)
-si_word_numbers = np.array(si_word_numbers)
-si_words = np.array(si_words)
-
-########################################
-## Make Word Idx
-## - map words to their index in vocab
-########################################
-
-train_val_word_idx = -np.ones((len(train_val_words)))
-for i in range(len(train_val_words)):
-    if train_val_words[i] in GRID_VOCAB:
-        train_val_word_idx[i] = GRID_VOCAB.index(train_val_words[i])
-
-si_word_idx = -np.ones((len(si_words)))
-for i in range(len(si_words)):
-    if si_words[i] in GRID_VOCAB:
-        si_word_idx[i] = GRID_VOCAB.index(si_words[i])
-
-########################################
-## Remove rows corresponding to words not in vocab ('a')
-########################################
-
-train_val_rows_to_keep = train_val_word_idx != -1
-
-train_val_dirs = train_val_dirs[train_val_rows_to_keep]
-train_val_word_numbers = train_val_word_numbers[train_val_rows_to_keep]
-train_val_words = train_val_words[train_val_rows_to_keep]
-train_val_word_idx = train_val_word_idx[train_val_rows_to_keep]
-
-si_rows_to_keep = si_word_idx != -1
-
-si_dirs = si_dirs[si_rows_to_keep]
-si_word_numbers = si_word_numbers[si_rows_to_keep]
-si_words = si_words[si_rows_to_keep]
-si_word_idx = si_word_idx[si_rows_to_keep]
-
-########################################
-## Assign Training and Testing Directories
-########################################
-
-# Train data - speakers speaking certain words
-# Test data - same speakers speaking other words
-# SI_Test data - different speakers speaking other words
-
-all_word_idx = np.arange(GRID_VOCAB_SIZE)
-
-# Choose words to keep in training data - training words
-np.random.seed(29)
-training_word_idx = np.random.choice(all_word_idx, train_num_of_words)
-
-# Make the rest of the words as part of testing data - testing words
-testing_word_idx = np.delete(all_word_idx, train_word_idx)
-
-# Choose those rows in data that contain training words
-training_data_idx = np.array([i for i in range(len(train_val_words)) if train_val_word_idx[i] in training_word_idx])
-
-# Choose the rest of the rows as testing data
-testing_data_idx = np.delete(np.arange(len(train_val_words)), training_data_idx)
-
-# Choose those rows in data that contain training words
-si_in_vocab_data_idx = np.array([i for i in range(len(si_words)) if si_word_idx[i] in training_word_idx])
-
-# Choose the rest of the rows as testing data
-si_oov_data_idx = np.delete(np.arange(len(si_words)), si_in_vocab_data_idx)
-
-# TRAINING DATA
-training_dirs = train_val_dirs[training_data_idx]
-training_word_numbers = train_val_word_numbers[training_data_idx]
-training_words = train_val_words[training_data_idx]
-training_word_idx = training_word_idx[training_data_idx]
-
-# (SPEAKER-DEPENDENT) TESTING DATA
-testing_dirs = train_val_dirs[testing_data_idx]
-testing_word_numbers = train_val_word_numbers[testing_data_idx]
-testing_words = train_val_words[testing_data_idx]
-testing_word_idx = training_word_idx[testing_data_idx]
-
-# SPEAKER-INDEPENDENT IN-VOCAB DATA
-si_in_vocab_dirs = si_dirs[si_in_vocab_data_idx]
-si_in_vocab_word_numbers = si_word_numbers[si_in_vocab_data_idx]
-si_in_vocab_words = si_words[si_in_vocab_data_idx]
-si_in_vocab_word_idx = si_word_idx[si_in_vocab_data_idx]
-
-# SPEAKER-INDEPENDENT OOV DATA
-si_oov_dirs = si_dirs[si_oov_data_idx]
-si_oov_word_numbers = si_word_numbers[si_oov_data_idx]
-si_oov_words = si_words[si_oov_data_idx]
-si_oov_word_idx = si_word_idx[si_oov_data_idx]
-
-########################################
-## Make Training and Testing Data
-########################################
-
+training_dirs, training_word_numbers, training_words, \
+    training_word_idx, testing_dirs, testing_word_numbers, testing_words, \
+    testing_word_idx, si_in_vocab_dirs, si_in_vocab_word_numbers, \
+    si_in_vocab_words, si_in_vocab_word_idx, si_oov_dirs, \
+    si_oov_word_numbers, si_oov_words, si_oov_word_idx \
+    = make_train_test_siInV_siOOV_data(train_num_of_words)
 
 
 
