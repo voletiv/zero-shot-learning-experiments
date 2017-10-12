95c2fb1d43c63f7f9414e2bd240dcdb1547d88ef
Vikram Voleti <vikky2904@gmail.com>
2017-10-13 04:09:43 +0530

    Attributes

--- GRIDcorpus/grid_attributes.py
+++ GRIDcorpus/grid_attributes.py
@@ -1,17 +1,74 @@
 # GET GRIDCORPUS ATTRIBUTES
 
-from grid_params import *
-from grid_functions import *
+from grid_attributes_functions import *
+
+attributes = {}
+
+########################################
+# ATTRIBUTES
+########################################
+
+# Speaker identity
+
+# Duration of word
+
+# Presence of bilabials in word
+
+# Mouth-to-face ratio
+
+# Pose
+
+# Range of mouth movement??
+
+
+# Audio freq - L, M, H
 
 ########################################
 # Get FULL Data
 ########################################
 
 train_val_dirs, train_val_word_numbers, train_val_word_idx, \
-    = load_GRIDcorpus_speakers_dirs_wordnums_wordidx_lists(TRAIN_VAL_SPEAKERS_LIST)
+    = load_GRIDcorpus_speakers_dirs_wordnums_wordidx_lists(TRAIN_VAL_SPEAKERS_LIST, GRID_DATA_DIR, GRID_VOCAB_FULL)
 
 si_dirs, si_word_numbers, si_word_idx \
-    = load_GRIDcorpus_speakers_dirs_wordnums_wordidx_lists(SI_SPEAKERS_LIST)
+    = load_GRIDcorpus_speakers_dirs_wordnums_wordidx_lists(SI_SPEAKERS_LIST, GRID_DATA_DIR, GRID_VOCAB_FULL)
+
+########################################
+# ATTR - speaker identity
+########################################
+
+train_val_speaker_identity = []
+for speaker in tqdm.tqdm(sorted((TRAIN_VAL_SPEAKERS_LIST))):
+    speaker_dir = os.path.join(
+            GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
+    # List of all videos for each speaker
+    vid_dirs_list = sorted(glob.glob(os.path.join(speaker_dir, '*/')))
+    # Append training directories
+    for vid_dir in vid_dirs_list:
+        for word_num in range(WORDS_PER_VIDEO):
+            train_val_speaker_identity.append(speaker)
+
+train_val_speaker_identity = np.array(train_val_speaker_identity, dtype=int)
+
+attributes['train_val_speaker_identity'] = train_val_speaker_identity
+np.save("attributes", attributes)
+
+si_speaker_identity = []
+for speaker in tqdm.tqdm(sorted((SI_SPEAKERS_LIST))):
+    speaker_dir = os.path.join(
+            GRID_DATA_DIR, 's' + '{0:02d}'.format(speaker))
+    # List of all videos for each speaker
+    vid_dirs_list = sorted(glob.glob(os.path.join(speaker_dir, '*/')))
+    # Append training directories
+    for vid_dir in vid_dirs_list:
+        for word_num in range(WORDS_PER_VIDEO):
+            si_speaker_identity.append(speaker)
+
+si_speaker_identity = np.array(si_speaker_identity, dtype=int)
+
+attributes['si_speaker_identity'] = si_speaker_identity
+np.save("attributes", attributes)
+
 
 ########################################
 # Load LipReader
@@ -20,22 +77,228 @@ si_dirs, si_word_numbers, si_word_idx \
 LSTMLipreaderModel, LSTMLipreaderEncoder = load_LSTM_lipreader_and_encoder()
 
 ########################################
-# ATTRIBUTES
+# LipReader predictions, word durations
 ########################################
 
-# Mouth-to-face ratio
+lipreader_preds = {}
 
-# Pose
+train_val_lipreader_pred_word_idx = np.zeros(len(train_val_dirs), dtype=int)
+train_val_lipreader_preds_correct_or_wrong = np.zeros(len(train_val_dirs), dtype=bool)
+make_LSTMlipreader_predictions(train_val_lipreader_pred_word_idx,
+                               train_val_lipreader_preds_correct_or_wrong,
+                               train_val_dirs,
+                               train_val_word_numbers,
+                               train_val_word_idx,
+                               LSTMLipreaderModel,
+                               GRID_VOCAB_FULL,
+                               0)
 
-# Duration of word
+lipreader_preds['train_val_lipreader_pred_word_idx'] = train_val_lipreader_preds
+lipreader_preds['train_val_lipreader_preds_correct_or_wrong'] = train_val_lipreader_preds_correct_or_wrong
+np.save('lipreader_preds', lipreader_preds)
 
-# Presence of bilabials
 
-# Range of mouth movement??
+si_lipreader_pred_word_idx = np.zeros(len(si_dirs), dtype=int)
+si_lipreader_preds_correct_or_wrong = np.zeros(len(si_dirs), dtype=bool)
+make_LSTMlipreader_predictions(si_lipreader_pred_word_idx,
+                               si_lipreader_preds_correct_or_wrong,
+                               si_dirs,
+                               si_word_numbers,
+                               si_word_idx,
+                               LSTMLipreaderModel,
+                               GRID_VOCAB_FULL,
+                               0)
+
+lipreader_preds['si_lipreader_pred_word_idx'] = si_lipreader_pred_word_idx
+lipreader_preds['si_lipreader_preds_correct_or_wrong'] = si_lipreader_preds_correct_or_wrong
+np.save('lipreader_preds', lipreader_preds)
 
 
 ########################################
-# ATTRIBUTES
+# ATTR - Word durations
+########################################
+
+train_val_word_durations = np.zeros(len(train_val_dirs), dtype=int)
+for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(train_val_dirs, train_val_word_numbers, train_val_word_idx)), total=len(train_val_dirs)):
+    alignFile = vidDir[:-1] + '.align'
+    # Word-Time data
+    wordTimeData = open(alignFile).readlines()
+    # Get the max time of the video
+    maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+    # Remove Silent and Short Pauses
+    for line in wordTimeData:
+        if 'sil' in line or 'sp' in line:
+            wordTimeData.remove(line)
+    # Find the start and end frame for this word
+    wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                0]) / maxClipDuration * FRAMES_PER_VIDEO)
+    wordEndFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                             1]) / maxClipDuration * FRAMES_PER_VIDEO)
+    train_val_word_durations[i] = wordEndFrame - wordStartFrame + 1
+
+attributes['train_val_word_durations'] = train_val_word_durations
+np.save("attributes", attributes)
+
+si_word_durations = np.zeros(len(si_dirs), dtype=int)
+for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(si_dirs, si_word_numbers, si_word_idx)), total=len(si_dirs)):
+    alignFile = vidDir[:-1] + '.align'
+    # Word-Time data
+    wordTimeData = open(alignFile).readlines()
+    # Get the max time of the video
+    maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+    # Remove Silent and Short Pauses
+    for line in wordTimeData:
+        if 'sil' in line or 'sp' in line:
+            wordTimeData.remove(line)
+    # Find the start and end frame for this word
+    wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                0]) / maxClipDuration * FRAMES_PER_VIDEO)
+    wordEndFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                             1]) / maxClipDuration * FRAMES_PER_VIDEO)
+    si_word_durations[i] = wordEndFrame - wordStartFrame + 1
+
+attributes['si_word_durations'] = si_word_durations
+np.save("attributes", attributes)
+
+
+########################################
+# ATTR - Head pose estimations
+########################################
+
+# Creating fileNamesList.txt
+
+# TRAIN_VAL
+train_val_frame_file_names = []
+for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(train_val_dirs, train_val_word_numbers, train_val_word_idx)), total=len(train_val_dirs)):
+    alignFile = vidDir[:-1] + '.align'
+    # Word-Time data
+    wordTimeData = open(alignFile).readlines()
+    # Get the max time of the video
+    maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+    # Remove Silent and Short Pauses
+    for line in wordTimeData:
+        if 'sil' in line or 'sp' in line:
+            wordTimeData.remove(line)
+    # Find the start and end frame for this word
+    wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                0]) / maxClipDuration * FRAMES_PER_VIDEO)
+    wordEndFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                             1]) / maxClipDuration * FRAMES_PER_VIDEO)
+    # All mouth file names of video
+    frameFiles = sorted(glob.glob(os.path.join(vidDir, '*Mouth*.jpg')))
+    # Note the file names of the word
+    wordFrameFiles = frameFiles[wordStartFrame:wordEndFrame + 1]
+    # Loop
+    for f, wordFrameFile in enumerate(wordFrameFiles):
+        # File names
+        train_val_frame_file_names.append(wordFrameFile.replace('Mouth', 'Frame'))
+
+with open("frame_names_list_train_val.txt", 'w') as f:
+    f.write('\n'.join(train_val_frame_file_names))
+
+# SI
+si_frame_file_names = []
+for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(si_dirs, si_word_numbers, si_word_idx)), total=len(si_dirs)):
+    alignFile = vidDir[:-1] + '.align'
+    # Word-Time data
+    wordTimeData = open(alignFile).readlines()
+    # Get the max time of the video
+    maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+    # Remove Silent and Short Pauses
+    for line in wordTimeData:
+        if 'sil' in line or 'sp' in line:
+            wordTimeData.remove(line)
+    # Find the start and end frame for this word
+    wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                0]) / maxClipDuration * FRAMES_PER_VIDEO)
+    wordEndFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                             1]) / maxClipDuration * FRAMES_PER_VIDEO)
+    # All mouth file names of video
+    frameFiles = sorted(glob.glob(os.path.join(vidDir, '*Mouth*.jpg')))
+    # Note the file names of the word
+    wordFrameFiles = frameFiles[wordStartFrame:wordEndFrame + 1]
+    # Loop
+    for f, wordFrameFile in enumerate(wordFrameFiles):
+        # File names
+        si_frame_file_names.append(wordFrameFile.replace('Mouth', 'Frame'))
+
+with open("frame_names_list_si.txt", 'w') as f:
+    f.write('\n'.join(si_frame_file_names))
+
+'''
+cd /home/voletiv/GitHubRepos/gazr/build
+./benchmark_head_pose_estimation_multiple_frames ../../lipreading-in-the-wild-experiments/shape-predictor/shape_predictor_68_face_landmarks.dat ../../zero-shot-learning-experiments/GRIDcorpus/frame_names_list_train_val.txt > head_poses_train_val.txt
+./benchmark_head_pose_estimation_multiple_frames ../../lipreading-in-the-wild-experiments/shape-predictor/shape_predictor_68_face_landmarks.dat ../../zero-shot-learning-experiments/GRIDcorpus/frame_names_list_si.txt > head_poses_si.txt
+'''
+
+# TRAIN_VAL
+train_val_head_poses = []
+with open('/home/voletiv/GitHubRepos/gazr/build/head_poses_train_val.txt', 'r') as f:
+    for line in f:
+        if 'Head pose' in line:
+            train_val_head_poses.append(line)
+
+attributes['train_val_head_poses'] = train_val_head_poses
+np.save("attributes", attributes)
+
+# SI
+si_head_poses = []
+with open('/home/voletiv/GitHubRepos/gazr/build/head_poses_si.txt', 'r') as f:
+    for line in f:
+        if 'Head pose' in line:
+            si_head_poses.append(line)
+
+attributes['si_head_poses'] = si_head_poses
+np.save("attributes", attributes)
+
+
+
+
+
+
+########################################
+# Get ATTRIBUTES
+########################################
+
+train_val_dirs, train_val_word_numbers, train_val_word_idx, \
+    = load_GRIDcorpus_speakers_dirs_wordnums_wordidx_lists(TRAIN_VAL_SPEAKERS_LIST, GRID_DATA_DIR, GRID_VOCAB_ZSL)
+
+
+def make_GRIDcorpus_features_and_one_hot_words_using_syncnet(dirs,
+                                                             word_numbers,
+                                                             word_idx,
+                                                             lipreaderEncoder):
+    features = np.zeros((len(dirs), 128))
+    one_hot_words = np.zeros((len(dirs), GRID_VOCAB_SIZE))
+    detector, predictor = load_detector_and_predictor()
+    # For each data point
+    for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(dirs, word_numbers, word_idx)), total=len(dirs)):
+        # GET SEQUENCE OF FRAMES
+        # align file
+        alignFile = vidDir[:-1] + '.align'
+        # Word-Time data
+        wordTimeData = open(alignFile).readlines()
+        # Get the max time of the video
+        maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+        # Remove Silent and Short Pauses
+        for line in wordTimeData:
+            if 'sil' in line or 'sp' in line:
+                wordTimeData.remove(line)
+        # Find the start and end frame for this word
+        wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                    0]) / maxClipDuration * FRAMES_PER_VIDEO)
+        wordEndFrame = math.ceil(int(wordTimeData[wordNum].split(' ')[
+                                 1]) / maxClipDuration * FRAMES_PER_VIDEO)
+        # All frame file names of video
+        frameFiles = sorted(glob.glob(os.path.join(vidDir, '*Frame*.jpg')))
+        # Note the file names of the word
+        wordFrameFiles = frameFiles[wordStartFrame:wordEndFrame + 1]
+        # 
+
+
+
+########################################
+# BILABIALS
 ########################################
 
 bilabials = {}
@@ -90,95 +353,3 @@ bilabials['x'] = 0
 bilabials['y'] = 0
 bilabials['z'] = 0
 bilabials['zero'] = 0
-
-
-########################################
-# Get ATTRIBUTES
-########################################
-
-def make_GRIDcorpus_features_and_one_hot_words_using_syncnet(dirs,
-                                                             word_numbers,
-                                                             word_idx,
-                                                             lipreaderEncoder):
-    features = np.zeros((len(dirs), 128))
-    one_hot_words = np.zeros((len(dirs), GRID_VOCAB_SIZE))
-    detector, predictor = load_detector_and_predictor()
-    # For each data point
-    for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(dirs, word_numbers, word_idx)), total=len(dirs)):
-        # GET SEQUENCE OF FRAMES
-        # align file
-        alignFile = vidDir[:-1] + '.align'
-        # Word-Time data
-        wordTimeData = open(alignFile).readlines()
-        # Get the max time of the video
-        maxClipDuration = float(wordTimeData[-1].split(' ')[1])
-        # Remove Silent and Short Pauses
-        for line in wordTimeData:
-            if 'sil' in line or 'sp' in line:
-                wordTimeData.remove(line)
-        # Find the start and end frame for this word
-        wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
-                                    0]) / maxClipDuration * FRAMES_PER_VIDEO)
-        wordEndFrame = math.ceil(int(wordTimeData[wordNum].split(' ')[
-                                 1]) / maxClipDuration * FRAMES_PER_VIDEO)
-        # All frame file names of video
-        frameFiles = sorted(glob.glob(os.path.join(vidDir, '*Frame*.jpg')))
-        # Note the file names of the word
-        wordMouthFiles = frameFiles[wordStartFrame:wordEndFrame + 1]
-
-
-
-
-
-# a
-# again
-# at
-# b
-# bin
-# blue
-# by
-# c
-# d
-# e
-# eight
-# f
-# five
-# four
-# g
-# green
-# h
-# i
-# in
-# j
-# k
-# l
-# lay
-# m
-# n
-# nine
-# now
-# o
-# one
-# p
-# place
-# please
-# q
-# r
-# red
-# s
-# set
-# seven
-# six
-# soon
-# t
-# three
-# two
-# u
-# v
-# white
-# with
-# x
-# y
-# z
-# zero
-
--- GRIDcorpus/grid_attributes_functions.py
+++ GRIDcorpus/grid_attributes_functions.py
@@ -1,6 +1,74 @@
 # GET GRIDCORPUS ATTRIBUTES
 
+import dlib
+import glob
+import math
+import numpy as np
+import tqdm
+
+from grid_functions import *
+
+def make_LSTMlipreader_predictions(lipreader_pred_word_idx,
+                                   lipreader_preds_correct_or_wrong,
+                                   word_durations,
+                                   dirs,
+                                   word_numbers,
+                                   word_idx,
+                                   lipreader,
+                                   grid_vocab=GRID_VOCAB_FULL,
+                                   startNum=0):
+    # dirs = train_val_dirs
+    # word_numbers = train_val_word_numbers
+    # word_idx = train_val_word_idx
+    detector, predictor = load_detector_and_predictor()
+    # For each data point
+    for i, (vidDir, wordNum, wordIndex) in tqdm.tqdm(enumerate(zip(dirs, word_numbers, word_idx)), total=len(dirs)):
+        if i < startNum:
+            continue
+        # GET SEQUENCE OF FRAMES
+        # align file
+        alignFile = vidDir[:-1] + '.align'
+        # Word-Time data
+        wordTimeData = open(alignFile).readlines()
+        # Get the max time of the video
+        maxClipDuration = float(wordTimeData[-1].split(' ')[1])
+        # Remove Silent and Short Pauses
+        for line in wordTimeData:
+            if 'sil' in line or 'sp' in line:
+                wordTimeData.remove(line)
+        # Find the start and end frame for this word
+        wordStartFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                    0]) / maxClipDuration * FRAMES_PER_VIDEO)
+        wordEndFrame = math.floor(int(wordTimeData[wordNum].split(' ')[
+                                 1]) / maxClipDuration * FRAMES_PER_VIDEO)
+        # Word duration
+        word_durations[i] = wordEndFrame - wordStartFrame + 1
+        # All mouth file names of video
+        mouthFiles = sorted(glob.glob(os.path.join(vidDir, '*Mouth*.jpg')))
+        # Note the file names of the word
+        wordMouthFiles = mouthFiles[wordStartFrame:wordEndFrame + 1]
+        # Initialize the array of images for this word
+        wordImages = np.zeros((1, FRAMES_PER_WORD, NUM_OF_MOUTH_PIXELS))
+        # For each frame of this word
+        for f, wordMouthFrame in enumerate(wordMouthFiles[:FRAMES_PER_WORD]):
+            # in reverse order of frames. eg. If there are 7 frames:
+            # 0 0 0 0 0 0 0 7 6 5 4 3 2 1
+            wordImages[0][-f - 1] = np.reshape(cv2.imread(wordMouthFrame,
+                                                          0) / 255., (NUM_OF_MOUTH_PIXELS,))
+        # MAKE PREDICTION
+        lipreader_pred_word_idx[i] = np.argmax(lipreader.predict(wordImages))
+        lipreader_preds_correct_or_wrong[i] = lipreader_pred_word_idx[i] == wordIndex
 
 
-
+def load_detector_and_predictor(verbose=False):
+    try:
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(SHAPE_PREDICTOR_PATH)
+        if verbose:
+            print("Detector and Predictor loaded. (load_detector_and_predictor)")
+        return detector, predictor
+    # If error in SHAPE_PREDICTOR_PATH
+    except RuntimeError:
+        raise ValueError("\n\nERROR: Wrong Shape Predictor .dat file path - " + \
+            SHAPE_PREDICTOR_PATH, "(load_detector_and_predictor)\n\n")
 
